5c029245c19c96e7114687b0cde45e1a8ad46ac8
aplicationqv <142430234+aplicationqv@users.noreply.github.com>
2023-10-04 12:43:48 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,8 @@
+
+// Con .offsetWidth podemos saber que tamaño tiene el elemento
+
 const botonMenu = () => {
 
-    const contenedorMenu = document.querySelector('#menu-lateral')
-    const contenedorLogo = document.querySelector('#logo')
     const contenedorPagina = document.querySelector('#pagina')
-
-    contenedorMenu.classList.toggle('active')
-    contenedorLogo.classList.toggle('active')
     contenedorPagina.classList.toggle('active')
-
 }
