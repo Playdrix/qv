cc3721610c3a233857e679f7010f89fa6368495a
aplicationqv <142430234+aplicationqv@users.noreply.github.com>
2023-10-06 15:59:27 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -14,3 +14,34 @@ window.addEventListener('resize', function() {
         contenedorPagina.classList.remove('active')
     }
 })
+
+
+
+
+const botonAplicaciones = document.getElementById('aplicaciones-boton')
+const botonWeb = document.getElementById('web-boton')
+const botonComentarios = document.getElementById('comentarios-boton')
+
+const contenedorAplicaciones = document.getElementById('pagina-aplicaciones')
+const contenedorWeb = document.getElementById('pagina-web')
+const contenedorComentarios = document.getElementById('pagina-comentarios')
+
+
+botonAplicaciones.addEventListener('click', function() {
+    contenedorAplicaciones.classList.add('active')
+    contenedorWeb.classList.remove('active')
+    contenedorComentarios.classList.remove('active')
+
+})
+
+botonWeb.addEventListener('click', function() {
+    contenedorWeb.classList.add('active')
+    contenedorAplicaciones.classList.remove('active')
+    contenedorComentarios.classList.remove('active')
+})
+
+botonComentarios.addEventListener('click', function() {
+    contenedorComentarios.classList.add('active')
+    contenedorAplicaciones.classList.remove('active')
+    contenedorWeb.classList.remove('active')
+})
